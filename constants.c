86eef920fbec073f5cca186e2d350bb8c8965461
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-05-29 12:02:49 +0200

    Usage des constantes

---
+++ constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    /* const int MYCONSTANT;
+    *  MYCONSTANT = 5;
+    *  génère une erreur 
+    */
+
+    const int MYCONSTANT = 5;
+    printf("%d\n", MYCONSTANT); // Affiche 5
+
+    // MYCONSTANT = 10; Génère une erreur
+}
