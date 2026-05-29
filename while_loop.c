a857133d905d781084d2ea01d2120063aef0011b
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-05-29 12:47:20 +0200

    implémentation de la boucle while

---
+++ while_loop.c
@@ -0,0 +1,23 @@
+#include <stab.h>
+#include <stdbool.h> 
+
+int main(){
+
+    int i = 0;
+
+    while (i < 5) {
+        printf("%d\n", i);
+        i++;
+    }
+
+    int countdown = 3;
+
+    while (countdown >= 0) {
+        printf("%d\n", countdown);
+        countdown--;
+    }
+    
+    printf("Happy New Year!!\n");
+
+    
+}
