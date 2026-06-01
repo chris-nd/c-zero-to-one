1ee1fb2b36939ab818c90bfbe007a94755352359
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-06-01 12:34:43 +0200

    Implémentation des fonctions mathématiques au travers du fichier d'entête <math.h>

---
+++ math_functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    
+    printf("%f\n", sqrt(16));
+    printf("%f\n", pow(2, 4));
+    printf("%f\n", floor(3.7));
+    printf("%f\n", ceil(3.2));
+    printf("%f\n", round(3.7));
+    printf("%f\n", round(3.2));
+    return 0;
+}
