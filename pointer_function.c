3f6b9eae6209f9cd20e65c88f726caa9acc3b4b2
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-06-20 20:05:51 +0200

    Update

--- pointer_function.c
+++ pointer_function.c
@@ -4,36 +4,38 @@
 int (*ptr)(int, int);
 
 // Déclaration de fonction
-void add() { printf("Add\n"); }
-void subtract() { printf("Subtract\n"); }
-void multiply() { printf("Multiply\n"); }
+int add1(int a, int b);
+void add2();
+void subtract();
+void multiply();
 
 int main() {
 
-    ptr = add; // Le nom de fonction représente déjà son adresse en mémoire
-    ptr = &add;
-    return 0;
+    ptr = add1; // Le nom de fonction représente déjà son adresse en mémoire
+    ptr = &add1; // correspond à (ptr = add1)
 
     // Appel de la fonction via le pointeur
-    int result = ptr(5, 3);
-    printf("Result= %d\n", result); // Affiche: Result= 8
-    int result = (*ptr)(5, 3);
-    printf("Result= %d\n", result); // Affiche: Result= 8
+    int result1 = ptr(5, 3);
+    printf("Result= %d\n", result1); // Affiche: Result= 8
+    int result2 = (*ptr)(5, 3); // correspond à (ptr(5, 3))
+    printf("Result= %d\n", result2); // Affiche: Result= 8
     
     // Tableau de pointeurs de fonction
-    void (*operations[3])() = { add, subtract, multiply };
+    void (*operations[3])() = { add2, subtract, multiply };
     for (int i = 0; i < 3; i++) {
       operations[i]();
     }
+
+    return 0;
 }
 
 // Définition de la fonction
-int add(int a, int b) {
+
+int add1(int a, int b) {
     return a + b;
 }
 
-
-int main() {
-  return 0;
-}
+void add2() { printf("Add\n"); }
+void subtract() { printf("Subtract\n"); }
+void multiply() { printf("Multiply\n"); }
 
